e2aba738b56c0d096ff0b0e1df6f0302be124f65
oudream <oudream@126.com>
2020-02-20 14:55:36 +0800

    Update nginx.sh

--- man/nginx.sh
+++ man/nginx.sh
@@ -1,5 +1,92 @@
 #!/usr/bin/env bash
 
+# news release
+open http://hg.nginx.org/nginx/
+open http://nginx.org/en/docs/
+
+
+# 查看安装了哪些模块,输出 NGINX 各文件夹的路径
+nginx -V
+
+
+# windows
+http://nginx.org/download/nginx-1.16.1.zip
+git clone https://github.com/oudream/nginx-v1.9.7
+# config
+./conf/nginx.conf # 默认配置
+
+
+# ubuntu
+apt install nginx
+/etc/nginx/nginx.conf # 默认配置
+
+
+# alpine
+apk add nginx
+/etc/nginx/nginx.conf # 默认配置
+
+
+# macos osx
+brew install nginx
+/usr/local/etc/nginx/nginx.conf # 默认配置
+
+
+### nginx
+nginx -c "/ddd/web/nginx/conf-limi/nginx.conf"
+nginx -t -c "/ddd/web/nginx/conf-limi/nginx.conf" # 只测试配置文件
+
+
+# 启动 nginx
+nginx
+
+# 停止 nginx
+nginx -s stop
+# or
+nginx -s quit
+
+
+# 重载 nginx 配置
+nginx -s reload
+# or
+nginx -s quit
+
+
+# 指定配置文件
+nginx -c /usr/local/nginx/conf/nginx.conf
+
+# 检查配置文件是否正确
+nginx -t /usr/local/nginx/conf/nginx.conf
+
+
+# 帮助信息
+nginx -h
+#or
+nginx -?
+
+
+# 查看 Nginx 版本
+nginx -v
+# or
+nginx -V
+
+
+# 获得 pid
+ps aux | grep nginx
+# or
+cat /path/to/nginx.pid
+
+
+# 从容停止 nginx，等所有请求结束后关闭服务
+ps aux | grep nginx
+kill -QUIT  pid
+
+
+# nginx 快速停止命令，立刻关闭进程
+ps aux | grep nginx
+kill -TERM pid
+
+
+
 nginx -s quit
 #  stop — fast shutdown
 #  quit — graceful shutdown
@@ -34,16 +121,16 @@ open http://nginx.org/en/docs/beginners_guide.html
 # command-line parameters
 open http://nginx.org/en/docs/switches.html
 
-
 # download
 open https://nginx.org/download/
+open http://nginx.org/en/linux_packages.html
 
-# NGINX Ingress Controller for Kubernetes
+# nginx Cookbook
 open https://github.com/sous-chefs/nginx
 # NGINX-based Media Streaming Server
 open https://github.com/arut/nginx-rtmp-module
 
-# cookbook
+# tutorials
 open https://github.com/openresty/nginx-tutorials
 open http://openresty.org/download/agentzh-nginx-tutorials-en.html
 open http://blog.sina.com.cn/openresty
@@ -56,3 +143,8 @@ open https://github.com/jwilder/nginx-proxy
 
 # lua
 open https://github.com/openresty/lua-nginx-module
+
+### doc
+# http://nginx.org/en/docs/
+# https://docs.nginx.com/
+# https://juejin.im/post/5d81906c518825300a3ec7ca
