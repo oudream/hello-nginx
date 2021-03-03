c3dc3c9c011b802af6343f2fdf8b7c5e3c5020f7
oudream <oudream@126.com>
2021-03-03 15:02:33 +0800

    up

---
+++ hello/readme.sh
@@ -0,0 +1,10 @@
+
+### ginx cmake
+# https://github.com/i-rinat/nginx-cmake
+# https://github.com/changyy/nginx-cmake
+# https://tylersguides.com/guides/installing-nginx-from-source-on-centos-7/
+# https://github.com/nginx/nginx/releases/tag/release-1.11.6
+# https://docs.nginx.com/nginx/admin-guide/installing-nginx/installing-nginx-open-source/
+# https://hub.docker.com/r/centos/nginx-18-centos7/
+
+
